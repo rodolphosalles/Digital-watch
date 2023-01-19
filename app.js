e101d3ed4fd533ffd2de510b15a38f11c7240200
rodolphosalles <104856188+rodolphosalles@users.noreply.github.com>
2023-01-19 12:00:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const clockContainer = document.querySelector('.clock-container')
-
 const formatTimeUnit = (unit) => {
   return String(unit).length === 1 ? `0${unit}` : unit;
-}
+};
+
+const clockContainer = document.querySelector(".clock-container");
 
 const updateClock = () => {
-  const present = new Date()
-  const hours = present.getHours()
-  const minutes = present.getMinutes()
-  const seconds = present.getSeconds()
+  const present = new Date();
+  const hours = present.getHours();
+  const minutes = present.getMinutes();
+  const seconds = present.getSeconds();
 
-  clockContainer.innerHTML =  `
+  clockContainer.innerHTML = `
     <span>${formatTimeUnit(hours)}</span> :
     <span>${formatTimeUnit(minutes)}</span> :
     <span>${formatTimeUnit(seconds)}</span>
-  `
-}
-setInterval(updateClock, 1000)
+  `;
+};
+setInterval(updateClock, 1000);
