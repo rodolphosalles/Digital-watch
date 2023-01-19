d378165f37acf9f62d661e4df75aaccb4701a6cb
rodolphosalles <104856188+rodolphosalles@users.noreply.github.com>
2023-01-19 11:53:35 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const clockContainer = document.querySelector('.clock-container')
+
+const updateClock = () => {
+  const present = new Date()
+  const hours = present.getHours()
+  const minutes = present.getMinutes()
+  const seconds = present.getSeconds()
+
+  clockContainer.innerHTML =  `
+    <span>${formatTimeUnit(hours)}</span> :
+    <span>${formatTimeUnit(minutes)}</span> :
+    <span>${formatTimeUnit(seconds)}</span>
+  `
+}
+setInterval(updateClock, 1000)
